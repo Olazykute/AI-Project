b11910110338440a380f9a6e097a0732ae843412
[Olazykute] <[eryzew@gmail.com]>
2024-03-18 10:54:39 +0100

    Show correlated columns

--- Projet IA.py
+++ Projet IA.py
@@ -1,9 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import polars as pl
+import pandas as pd
 
 data_0 = pl.read_csv("Features_by_window_size/sero_features_4.csv")
 print(data_0)
+
+""" 
+plt.hist(data_0[:,"Target"],bins=10)
+plt.xlabel("Target")
+plt.show()
+"""
+
+# Calculate correlation matrix
+corr_matrix = data_0.corr()
+
+# Define correlation threshold
+threshold = 0.7
+
+# Extract highly correlated columns
+highly_correlated_cols = []
+
+for i, col in enumerate(data_0.columns):
+    for j in range(i + 1, len(data_0.columns)):
+        corr_value = corr_matrix[(i, j)]
+        if abs(corr_value) >= threshold:
+            highly_correlated_cols.append((data_0.columns[i], data_0.columns[j]))
+
+print("Highly correlated columns:")
+for col_pair in highly_correlated_cols:
+    print(col_pair)
+    
 
 target = data_0[:,"Target"]
 x = np.linspace(0,target.len(),target.len())
