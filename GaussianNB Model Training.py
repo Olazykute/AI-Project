899e3b8cad53a21e515e87463557d723749f665b
[Kaden Corwel] <kadencorwel@gmail.com>
2024-03-28 18:13:46 +0100

    Cleaned GNB model training code in functions

--- GaussianNB Model Training.py
+++ GaussianNB Model Training.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report
@@ -11,51 +10,59 @@ import Projet_IA as P
 # data=pl.read_csv("Features_by_window_size/sero_features_4.csv")
 # print (data)
 
-X = P.data_0.filtered[:, 1:len(P.data_0.filtered)] #les caractéristiques
-y = P.data_0.filtered[:, 0]  #les résulats (classes)
+def data_transfer(df):
+    X = df[:, 1:len(df)] #les caractéristiques
+    y = df[:, 0]  #les résulats (classes)
+    X = X.to_numpy()
+    y = y.to_numpy()
+    return X, y
 
-X = X.to_numpy()
-y = y.to_numpy()
+def data_scaling(x):
+    scaler = StandardScaler()
+    x=scaler.fit_transform(x)  # Fit the scaler to your data and transform the matrix
+    return x
 
+def save_model(modele, nom):
+    dump( modele, nom+'.joblib')
+    return
+
+def load_model(nom):
+    modele=load(nom)
+    return modele
+
+def entrainement_modele(modele, X_train, y_train, X_test, y_test):
+    modele.fit(X_train, y_train) # Fitting is equal to training
+
+    y_pred = modele.predict(X_test)
+    y_pred2 = modele.predict(X_train)
+    print("precsion en test: ", accuracy_score(y_test, y_pred))
+    print("precsion en entrainement: ", accuracy_score(y_train, y_pred2))
+
+    report = classification_report(y_test, y_pred)
+    print(print("Classification Report on test:\n", report))
+    # Le classification report donne la précision du modèle par classe
+
+    # Perform cross-validation
+    cv_scores = cross_val_score(modele, X_test, y_test, cv=5)
+    print("Cross-Validation Scores:", cv_scores)
+    print("Mean Cross-Validation Score:", cv_scores.mean())
+    # La validation croisée teste le modèle 5 fois de suite dans l'ensemble de test.
+
+    return modele
+
+X, y = data_transfer(P.data_0.filtered)
+
+# Division du dataset en différentes parties
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# print(X_train)
 # Normalisation et standardisation
-scaler = StandardScaler()
-# Fit the scaler to your data and transform the matrix
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
+X_train = data_scaling(X_train)
+X_test = data_scaling(X_test)
 
-#print(X_train)
-
-modele = GaussianNB()
-modele.fit(X_train, y_train)
-
-y_pred = modele.predict(X_test)
-y_pred2 = modele.predict(X_train)
-print("precsion en test: ", accuracy_score(y_test, y_pred))
-print("precsion en entrainement: ", accuracy_score(y_train, y_pred2))
-
-report = classification_report(y_test, y_pred)
-print(print("Classification Report on test:\n", report))
-# Le classification report donne la précision du modèle par classe
-
-# Perform cross-validation
-cv_scores = cross_val_score(modele, X_test, y_test, cv=5)
-print("Cross-Validation Scores:", cv_scores)
-print("Mean Cross-Validation Score:", cv_scores.mean())
-# La validation croisée teste le modèle 5 fois de suite dans l'ensemble de test.
-
-
-# Inutile pour un GaussienNB
-#ValidationCurveDisplay.from_estimator(
-#   GaussianNB(kernel="linear"), X, y, param_name="", param_range=np.logspace(-7, 3, 10)
-#)
-plt.show()
-
-# ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+# GaussianNB_model=GaussianNB()
+GaussianNB_model=entrainement_modele(GaussianNB(), X_train, y_train, X_test, y_test)
 
 # enregistre le modèle dans un fichier joblib, dump pour enregistrer, load pour le charger
-#dump( modele, 'Vehicle_prediction_GaussianNB.joblib')
+save_model(GaussianNB_model, 'Vehicle_prediction_GaussianNB')
 
 # No need for hyperparameters in a GaussianNB model
