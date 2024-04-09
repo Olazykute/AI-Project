Corrected scaling method to improve accuracy of the models

Decision_Tree_Model_Training.py
@@ -14,8 +14,7 @@ print("Decision Tree Model")
 X, y = P.data_transfer(P.data_0.filtered)
 
 X_train, X_test, y_train, y_test = P.train_test_split(X, y, test_size=0.2)
-X_train = P.data_scaling(X_train)
-X_test = P.data_scaling(X_test)
+X_train, X_test = P.data_scaling(X_train, X_test)
 X = P.np.concatenate((X_train, X_test), axis=0)
 y = P.np.concatenate((y_train, y_test), axis=0)
 
@@ -31,10 +30,10 @@ model_clf,  prediction_test, prediction_train = P.training_model(DecisionTreeCla
 
 # P.save_model(model_clf, 'Vehicle_prediction_DecisionTree')
 
-""" 
+
 P.Model_Report(model_clf, X, y, y_test, prediction_test)
 P.disp_confusionMatrix(model_clf, y_test, prediction_test,'Confusion matrix for DecisionTreeClassifier model')
-"""
+
 
 """ 
 param_grid = {  # This part is to research the best parameters to maximize the model's accuracy

GaussianNB_Model_Training.py
@@ -11,8 +11,7 @@ X, y = P.data_transfer(P.data_0.filtered)
 X_train, X_test, y_train, y_test = P.train_test_split(X, y, test_size=0.2)
 
 # Normalisation and standardisation
-X_train = P.data_scaling(X_train)
-X_test = P.data_scaling(X_test)
+X_train, X_test = P.data_scaling(X_train, X_test)
 X = P.np.concatenate((X_train, X_test), axis=0)
 y = P.np.concatenate((y_train, y_test), axis=0)
 
@@ -31,5 +30,5 @@ end_time = time.perf_counter()
 print("Execution time: ", end_time - start_time)
 
 # Plot learning curve
-# P.plot_learning_curve(GaussianNB(), 'Learning Curve For GaussianNB Model', X, y, cv=5)
+P.plot_learning_curve(GaussianNB(), 'Learning Curve For GaussianNB Model', X, y, cv=5)
 

K-Neighbors_model.py
@@ -10,8 +10,7 @@ X, y = P.data_transfer(P.data_0.filtered)
 X_train, X_test, y_train, y_test = P.train_test_split(X, y, test_size=0.2)
 
 # Normalisation and standardisation
-X_train = P.data_scaling(X_train)
-X_test = P.data_scaling(X_test)
+X_train, X_test = P.data_scaling(X_train, X_test)
 X = P.np.concatenate((X_train, X_test), axis=0)
 y = P.np.concatenate((y_train, y_test), axis=0)
 

Projet_IA.py
@@ -8,7 +8,6 @@ from joblib import dump, load
 from sklearn.preprocessing import StandardScaler
 
 
-
 class Data:
     def __init__(self, path):
         self.raw = pl.read_csv(path)
@@ -87,11 +86,12 @@ def data_transfer(df):
     return X, y
 
 
-def data_scaling(x):
+def data_scaling(x_train, x_test):
     scaler = StandardScaler()
     # Fit the scaler to your data and transform the matrix
-    x = scaler.fit_transform(x)
-    return x
+    x_tr = scaler.fit_transform(x_train)
+    x_tst = scaler.transform(x_test)
+    return x_tr, x_tst
 
 
 def save_model(model, nom):
@@ -115,7 +115,7 @@ def training_model(model, X_train, y_train, X_test, y_test):
     return model, y_pred, y_pred_train
 
 
-def Model_Report(model, X, y , y_test, y_pred):
+def Model_Report(model, X, y, y_test, y_pred):
 
     report = classification_report(y_test, y_pred)
     print(print("Classification Report on test:\n", report))
@@ -166,10 +166,11 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=None,
     plt.show()
     return plt
 
+
 def plot_gauss(data_gauss):
     plt.figure(figsize=(12, 12))
 
-    i=0
+    i = 0
     for col in data_gauss.columns:
         # Calculer la moyenne et l'écart type
         moy = data_gauss[col].mean()
@@ -177,19 +178,21 @@ def plot_gauss(data_gauss):
 
         # Création de la courbe de répartition gaussienne
         x = np.linspace(moy - 3*std, moy + 3*std, 100)
-        y = (1/(std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - moy) / std) ** 2)
+        y = (1/(std * np.sqrt(2 * np.pi))) * \
+            np.exp(-0.5 * ((x - moy) / std) ** 2)
 
         # Plot de la courbe de répartition gaussienne
         plt.subplot(4, 4, i + 1)
         i += 1
         plt.plot(x, y, color='red')
         plt.hist(data_gauss[col], bins=50, density=True, color='blue')
-        #plt.title(f'{col}')
+        # plt.title(f'{col}')
         plt.xlabel(col)
         plt.ylabel('Fréquence')
     plt.legend()
-    plt.tight_layout()        
+    plt.tight_layout()
     plt.show()
+
 
 # print(data_0.raw)
 """ 

Random_Forest_Model.py
@@ -13,8 +13,7 @@ print("Random Forest Model")
 X, y = P.data_transfer(P.data_0.filtered)
 
 X_train, X_test, y_train, y_test = P.train_test_split(X, y, test_size=0.2)
-X_train = P.data_scaling(X_train)
-X_test = P.data_scaling(X_test)
+X_train, X_test = P.data_scaling(X_train, X_test)
 X = P.np.concatenate((X_train, X_test), axis=0)
 y = P.np.concatenate((y_train, y_test), axis=0)
 
@@ -26,14 +25,15 @@ model_clf, prediction_test, prediction_train = P.training_model(RandomForestClas
     max_leaf_nodes=10, bootstrap=True),
     X_train, y_train, X_test, y_test)
 '''
-model_clf, prediction_test, prediction_train = P.training_model(
-    RandomForestClassifier(), X_train, y_train, X_test, y_test)
+model_clf, prediction_test, prediction_train = P.training_model(RandomForestClassifier(), X_train, y_train, X_test, y_test)
 # P.save_model(model_clf, 'Vehicle_prediction_RandomForest')
 
 """ 
 P.Model_Report(model_clf, X, y, y_test, prediction_test)
 P.disp_confusionMatrix(model_clf, y_test, prediction_test,'Confusion matrix for RandomForestClassifier model')
 """
+
+
 """
 tree_to_plot = model_clf.estimators_[5]
 P.plt.figure(figsize=(20, 10))
@@ -88,5 +88,4 @@ end_time = time.perf_counter()
 print("Execution time: ", end_time - start_time)
 
 # Plot learning curve
-P.plot_learning_curve(
-    model_clf, 'Learning Curve For Random Forest Model', X, y, cv=5)
+P.plot_learning_curve(model_clf, 'Learning Curve For Random Forest Model', X, y, cv=5)